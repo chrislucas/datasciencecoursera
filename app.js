988ecde7a530ca712e68dd67dbd2b718650b1e75
wiselabs <c_lucas1010@hotmail.com>
2015-03-04 07:22:53 -0300

    primeiro commit arquivo js de test

---
+++ app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+//http://expressjs.com/api.html#app.use
+//http://www.sitepoint.com/serving-static-files-with-node-js/
+app.use("/", express.static(__dirname + '/public'));
+
+var server = http.Server(app);
+
+//http://expressjs.com/api.html#middleware
+app.get('/', function(rq, rs) {
+	//console.log(rq);
+	//http://expressjs.com/api.html#res.sendFile
+	rs.sendFile(__dirname + '/public/index.html', function(err){
+		if(err) {
+			console.log(err);
+		}
+	});
+});
+
+server.listen(3000, function(){
+	console.log('Test Ajax JSUML');
+});
+
